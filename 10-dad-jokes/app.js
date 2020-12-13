8a689d16f484694036545a0b814ddcc83947bfb6
NikoReva <57528503+NikoReva@users.noreply.github.com>
2020-12-13 19:07:29 +0100

    Project #10: Dad Jokes

---
+++ 10-dad-jokes/app.js
@@ -0,0 +1,35 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('joke-btn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+	config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	}
+
+	const res = await fetch('https://icanhazdadjoke.com', config)
+
+	const data = await res.json()
+
+	jokeEl.innerHTML = data.joke
+}
+
+// function generateJoke() {
+// 	config = {
+// 		headers: {
+// 			Accept: 'application/json',
+// 		},
+// 	}
+
+// 	fetch('https://icanhazdadjoke.com', config)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			jokeEl.innerHTML = data.joke
+// 			console.log(data)
+// 		})
+// }
